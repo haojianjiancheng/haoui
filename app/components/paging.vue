d002482847dd2983939f2ef7423a14e3c3180d15
haojianjiancheng <854723516@qq.com>
2019-04-22 11:07:33 +0800

    更改了一些属性名字、函数名字，函数写法

--- app/components/paging.vue
+++ app/components/paging.vue
@@ -1,12 +1,12 @@
  <template>
     <ul class="paging">
-        <li :class="['page' , {'page-disabled' : presentNum === 1}]" @click="addNum(-1)">&lt;</li>
-        <li class="page" v-if="leftSpot" @click="addNum(-3)">...</li>
+        <li :class="['page' , {'page-disabled' : presentNum === 1}]" @click="addPageNum(-1)">&lt;</li>
+        <li class="page" v-if="leftSpot" @click="addPageNum(-3)">...</li>
         <li :class="['page' , {'page-active' : presentNum === item}]" v-for="item in page" :key="item" @click="outputPagNumRange(item)">
             {{item}}
         </li>
-        <li class="page" v-if="rightSpot" @click="addNum(3)">...</li>
-        <li :class="['page' , {'page-disabled' : presentNum === pageNum}]" @click="addNum(1)">&gt;</li>
+        <li class="page" v-if="rightSpot" @click="addPageNum(3)">...</li>
+        <li :class="['page' , {'page-disabled' : presentNum === pageNum}]" @click="addPageNum(1)">&gt;</li>
     </ul>
 </template>
 
@@ -39,10 +39,10 @@
             pageNum(){
                 return Math.ceil(this.termNum/this.rowNum)
             },
-            beginNum() {
+            beginTermNum() {
                 return (this.presentNum-1)*this.rowNum
             },
-            endNum(){
+            endTermNum(){
                 return Math.ceil(this.presentNum*this.rowNum)
             },
             page(){
@@ -54,14 +54,16 @@
                     for (let i = 0; i < pageButNum; i++) {
                         array[i] = i+1;
                     }
-                }else if(num>this.pageNum-ceil){
+                    return array
+                }
+                if(num>this.pageNum-ceil){
                     for (let i = 0; i < pageButNum; i++) {
                         array[i] = this.pageNum-pageButNum+i+1;
                     }
-                }else{
-                    for (let i = 0; i < pageButNum; i++) {
-                        array[i] = num+i+1-ceil;
-                    }
+                    return array
+                }
+                for (let i = 0; i < pageButNum; i++) {
+                    array[i] = num+i+1-ceil;
                 }
                 return array                
             },
@@ -75,19 +77,19 @@
         methods: {
             outputPagNumRange(num) {
                 this.presentNum = num;
-                this.$emit('listRange',this.beginNum,this.endNum)
+                this.$emit('listRange',this.beginTermNum,this.endTermNum)
             },
-            addNum(num) {
-                this.presentNum = this.presentNum+parseInt(num);
-                if(this.presentNum<1){
+            addPageNum(num) {
+                let changePageNum = this.presentNum+parseInt(num);
+                if(changePageNum<1){
                     this.outputPagNumRange(1)
                     return
                 }
-                if(this.presentNum>this.pageNum){
+                if(changePageNum>this.pageNum){
                     this.outputPagNumRange(this.pageNum)
                     return
                 }
-                this.$emit('listRange',this.beginNum,this.endNum)
+                this.outputPagNumRange(changePageNum)
             }
         },
     }
